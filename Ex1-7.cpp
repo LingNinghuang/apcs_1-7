123fd96ed4c55bf17557d6dce9ca08c14bfb0527
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-10 13:20:48 +0800

    Create Ex1-7.cpp

---
+++ Ex1-7.cpp
@@ -0,0 +1,11 @@
+//data types
+#include <stdio.h>
+int main() {
+    float myfloatnum = 20.23;
+    printf("%f\n", myfloatnum);
+    printf("%.1f\n", myfloatnum);
+    printf("%.2f\n", myfloatnum);
+    printf("%.3f\n", myfloatnum);
+    printf("%.4f", myfloatnum);
+    return 0;
+}
